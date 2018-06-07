1fb843c816062bdfe73b6e54cef9fae21455dc0b
Alexander Martetskiy <maretskiy@gmail.com>
2018-06-07 03:45:15 +0300

    Add initial versions of alternate layout for calculation - this is an example how this can be done

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var InputData = function() {
+        // TODO:
+        //  * get data from
+        //  * validate data
+        //  * post-process data, if required
+    };
+
+    var DisplayResults = function(calculation_results) {
+        // TODO:
+        //  * compose html with results
+    };
+
+    var Calculator = function(input_results) {
+        // TODO:
+        //  * transform input_results into calculation_results
+        //  * create new DisplayResults
+        //  * show DisplayResults
+    };
+
+    // TODO: bind events
+});
